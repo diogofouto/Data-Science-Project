0fd07c8ee2255a701f0da8392a15e524bdbba6a9
Diogo Fouto <diogo.fouto@protonmail.com>
2021-12-05 17:18:37 +0000

    Cleanup

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -1,22 +1,31 @@
 import pandas as pd
 import seaborn as sns
-from matplotlib.pyplot import show, savefig
+import numpy as np
+from matplotlib.pyplot import title, savefig, clf
+from pandas.plotting import register_matplotlib_converters
+
+
+def get_correlations(data, name, path):
+	correlation_matrix = data.corr()
+	sns.heatmap(correlation_matrix,  xticklabels=correlation_matrix.columns, yticklabels=correlation_matrix.columns, cmap='Blues', fmt='.3f')
+	title(name)
+	savefig(path, dpi=300, bbox_inches="tight")
+	clf()
+
 
 def main():
+	register_matplotlib_converters()
+
 	# Get correlations for 'Air Quality'
-	temp = pd.read_csv('data/air_quality_tabular.csv', index_col='FID', na_values='', parse_dates=False, infer_datetime_format=False)
+	temp = pd.read_csv('data/air_quality_tabular.csv', index_col='FID', na_values='', parse_dates=True, infer_datetime_format=True)
 	data = temp.apply(lambda x: pd.factorize(x)[0]) # Convert all var types to numerical
-	sns.heatmap(data.corr()).set(title="Correlation matrix for Air Quality\n")
-	savefig('images/lab1/correlation/air_quality_correlation_analysis.png', dpi=300, bbox_inches = "tight")
-	show()
+	get_correlations(data, 'Correlation Analysis for Air Quality\n', 'images/lab1/correlation/air_quality_correlation_analysis.png')
 
 
 	# Get correlations for 'NYC Collisions'
-	temp = pd.read_csv('data/NYC_collisions_tabular.csv', index_col='COLLISION_ID', na_values='', parse_dates=False, infer_datetime_format=False)
+	temp = pd.read_csv('data/NYC_collisions_tabular.csv', index_col='COLLISION_ID', na_values='', parse_dates=True, infer_datetime_format=True)
 	data = temp.apply(lambda x: pd.factorize(x)[0]) # Convert all var types to numerical
-	sns.heatmap(data.corr()).set(title="Correlation matrix for NYC Collisions\n")
-	savefig('images/lab1/correlation/NYC_collisions_correlation_analysis.png', dpi=300, bbox_inches = "tight")
-	show()
+	get_correlations(data, 'Correlation Analysis for NYC Collisions\n', 'images/lab1/correlation/NYC_collisions_correlation_analysis.png')
 
 
 if __name__ == '__main__':
--- granularity.py
+++ granularity.py
@@ -1,33 +1,41 @@
 from pandas import read_csv
-from ds_charts import get_variable_types, HEIGHT
-from matplotlib.pyplot import subplots, savefig
+from ds_charts import HEIGHT
+from matplotlib.pyplot import subplots, savefig, clf
+from pandas.plotting import register_matplotlib_converters
+import config as cfg
 
 
-def plot_histograms(data, bins):
+
+def plot_histograms(data, bins, path):
 	rows = len(data.columns)
 	cols = len(bins)
 
-	fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT))
+	fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
 	for i in range(rows):
 	    for j in range(cols):
 	        axs[i, j].set_title('Histogram for %s - %d bins'%(data.columns[i], bins[j]))
 	        axs[i, j].set_xlabel(data.columns[i])
 	        axs[i, j].set_ylabel('Nr records')
-	        axs[i, j].hist(data[data.columns[i]].values.tolist(), bins=bins[j])
+	        axs[i, j].hist(data[data.columns[i]].values.tolist(), bins=bins[j], edgecolor=cfg.LINE_COLOR, color=cfg.FILL_COLOR)
+
+	savefig(path, dpi=300, bbox_inches="tight")
+	clf()
 
 
 def main():
+	register_matplotlib_converters()
+	
 	# Get granularity for 'Air Quality'
-	data = read_csv('data/air_quality_tabular.csv', index_col='FID', na_values='', parse_dates=False, infer_datetime_format=False)
+	data = read_csv('data/air_quality_tabular.csv', index_col='FID', na_values='', parse_dates=True, infer_datetime_format=True)
 	bins = (10, 100)
-	plot_histograms(data, bins)
-	savefig('images/lab1/granularity/air_quality_granularity_study.png', dpi=300, bbox_inches = "tight")
+	path = 'images/lab1/granularity/air_quality_granularity_study.png'
+	plot_histograms(data, bins, path)
 
 	# Get granularity for 'NYC Collisions'
-	data = read_csv('data/NYC_collisions_tabular.csv', index_col='COLLISION_ID', na_values='', parse_dates=False, infer_datetime_format=False)
+	data = read_csv('data/NYC_collisions_tabular.csv', index_col='COLLISION_ID', na_values='', parse_dates=True, infer_datetime_format=True)
 	bins = (10, 100, 1000)
-	plot_histograms(data, bins)
-	savefig('images/lab1/granularity/NYC_collisions_granularity_study.png', dpi=300, bbox_inches = "tight")
+	path = 'images/lab1/granularity/NYC_collisions_granularity_study.png'
+	plot_histograms(data, bins, path)
 
 
 if __name__ == '__main__':
