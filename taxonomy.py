542ee1b4b843320952bb2e68d74b5c4b5928a5e1
Diogo Fouto <diogo.fouto@protonmail.com>
2021-12-11 09:47:23 +0000

    Added taxonomy encodings

--- taxonomy.py
+++ taxonomy.py
@@ -3,6 +3,45 @@ from pandas.plotting import register_matplotlib_converters
 from ds_charts import get_variable_types
 
 
+person_sex = {'U': 0, 'M': 1, 'F': 2}
+
+bodily_injury = {'Unknown': -1, 'Does Not Apply': 0, 'Entire Body': 1, 'Head': 2, 'Neck': 3, 'Chest': 4, 'Back': 5,
+					'Abdomen - Pelvis': 6, 'Arm': 7, 'Leg': 8, 'Face': 9, 'Eye': 10, 'Shoulder - Upper Arm': 11,
+					'Elbow-Lower-Arm-Hand': 12, 'Hip-Upper Leg': 13, 'Knee-Lower Leg Foot': 14}
+
+safety_equipment = {'Unknown': -1, 'None': 0, 'Air Bag Deployed': 1, 'Child Restraint': 2, 'Harness': 3, 'Helmet': 4, 'Lap Belt': 5, 
+					'Pads': 6, 'Stoppers': 7, 'Other': 8}
+
+
+def get_encoding(string, encoder):
+	encoding = -2
+
+	if isinstance(string, str) is False:
+		return encoding
+
+	for key, value in encoder.items():
+		if key in string:
+			if key == 'Arm' or key == 'Leg':
+				continue
+			elif encoding != -2 and encoder == safety_equipment:
+				encoding = int(str(encoding) + str(value))
+			elif encoding != -2 and encoder == bodily_injury:
+				break
+			else:
+				encoding = value
+
+	if encoding == -2:
+		raise ValueError("Encoding didn't work.")
+
+	return encoding
+
+
+def create_encoding_columns(data):
+	data['SAFETY_EQUIPMENT_ENCODING'] = data.apply(lambda row: get_encoding(row['SAFETY_EQUIPMENT'], safety_equipment), axis=1)
+	data['BODILY_INJURY_ENCODING'] = data.apply(lambda row: get_encoding(row['BODILY_INJURY'], bodily_injury), axis=1)
+	data['PERSON_SEX_ENCODING'] = data.apply(lambda row: get_encoding(row['PERSON_SEX'], person_sex), axis=1)
+
+
 def print_unique_symbolic_values(data):
 	variables = get_variable_types(data)['Symbolic']
 	if [] == variables:
@@ -23,15 +62,11 @@ def print_unique_symbolic_values(data):
 	if 'PERSON_ID' in variables:
 		variables.remove('PERSON_ID')
 
-	#print()
-	#print(variables)
-
 	data = data.drop(columns=[col for col in data if col not in variables])
 
 	for col in data:
 		print('\n' + col)
 		print(data[col].unique())
-
 	print()
 
 
@@ -39,13 +74,17 @@ def main():
 	register_matplotlib_converters()
 
 	# 'Air Quality'
-	data = pd.read_csv('data/air_quality_tabular.csv', index_col='FID', parse_dates=True, infer_datetime_format=True)
-	print_unique_symbolic_values(data)
+	#data = pd.read_csv('data/air_quality_tabular.csv', index_col='FID', parse_dates=True, infer_datetime_format=True)
+	#print_unique_symbolic_values(data)
 
 
 	# 'NYC Collisions'
 	data = pd.read_csv('data/NYC_collisions_tabular.csv', index_col='COLLISION_ID', parse_dates=True, infer_datetime_format=True)
-	print_unique_symbolic_values(data)
+	#print_unique_symbolic_values(data)
+	create_encoding_columns(data)
+	print(data['SAFETY_EQUIPMENT_ENCODING'])
+	print(data['BODILY_INJURY_ENCODING'])
+	print(data['PERSON_SEX_ENCODING'])
 
 
 
