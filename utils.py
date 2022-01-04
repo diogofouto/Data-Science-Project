94827e382969cc4cdbfe2a6eb3e86c2ec0b5ce4c
JFonseca25 <joao.b.fonseca@tecnico.ulisboa.pt>
2022-01-04 10:42:26 +0000

    changes to utils, and new images from chosen balancing

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from ds_charts import plot_evaluation_results
+from sklearn.model_selection import train_test_split
 
 AQ_FILENAME = 'data/air_quality'
 AQ_FILETAG = 'air_quality'
@@ -11,18 +12,20 @@ NYC_FILENAME = 'data/NYC_collisions'
 NYC_FILETAG = 'NYC_collisions'
 NYC_TARGET = 'PERSON_INJURY'
 
-def get_splits_and_labels(filename, target):
+def get_splits_tests_and_labels(filename, target):
     train: pd.DataFrame = pd.read_csv(f'{filename}_train.csv')
-    y_train: np.ndarray = train.pop(target).values
-    X_train: np.ndarray = train.values
-    labels = np.unique(y_train)
+    y: np.ndarray = train.pop(target).values
+    X: np.ndarray = train.values
+    labels = np.unique(y)
     labels.sort()
+
+    X_train, X_dev, y_train, y_dev = train_test_split(X, y, train_size=0.7, stratify=y)
 
     test: pd.DataFrame = pd.read_csv(f'{filename}_test.csv')
     y_test: np.ndarray = test.pop(target).values
     X_test: np.ndarray = test.values
 
-    return train, X_train, y_train, test, X_test, y_test, labels
+    return train, X_train, y_train, X_dev, y_dev, test, X_test, y_test, labels
 
 def get_model_evaluation(model, X_train, y_train, X_test, y_test, labels, file_tag, sufix):
     train_pred = model.predict(X_train)
