d0047a59763ecf301534d57b124b26afada8b3cc
Diogo Fouto <diogo.fouto@protonmail.com>
2022-01-16 16:37:49 +0000

    New fs

--- feature_selection.py
+++ feature_selection.py
@@ -7,7 +7,11 @@ from pandas.plotting import register_matplotlib_converters
 from ds_charts import bar_chart, get_variable_types, HEIGHT
 
 
-THRESHOLD = 0.9
+AQ_VAR_THRESHOLD = 1.5
+AQ_CORR_THRESHOLD = "TODO"
+
+NYC_VAR_THRESHOLD = 0.05
+NYC_CORR_THRESHOLD = 0.9
 
 
 def get_redundant_variables(corr_mtx, threshold: float) -> tuple[dict, pd.DataFrame]:
@@ -33,7 +37,7 @@ def get_variable_correlation(corr_mtx, titl):
 	figure(figsize=[10, 10])
 	heatmap(corr_mtx, xticklabels=corr_mtx.columns, yticklabels=corr_mtx.columns, annot=False, linecolor=cfg.LINE_COLOR, cmap='Blues')
 	title('Filtered Correlation Analysis')
-	savefig('images/lab6/feature_selection/' + titl +'_filtered_correlation_analysis_0.9.png', dpi=300, bbox_inches="tight")
+	savefig('images/lab6/feature_selection/' + titl +'_correlation_analysis.png', dpi=300, bbox_inches="tight")
 	clf() # cleanup
 
 
@@ -57,15 +61,15 @@ def select_low_variance(data: pd.DataFrame, threshold: float, titl) -> list:
 	lst_variances = []
 	for el in data.columns:
 		value = data[el].var()
-		if value >= threshold:
+		if value < threshold:
 			lst_variables.append(el)
 			lst_variances.append(value)
 
 	#print(len(lst_variables), lst_variables)
-	figure(figsize=[10, 4])
-	bar_chart(lst_variables, lst_variances, title='Variance analysis', xlabel='variables', ylabel='variance', rotation=45)
-	savefig('images/lab6/feature_selection/' + titl + '_filtered_variance_analysis.png', dpi=300, bbox_inches="tight")
-	clf() # cleanup
+	#figure(figsize=[10, 4])
+	#bar_chart(lst_variables, lst_variances, title='Variance analysis', xlabel='variables', ylabel='variance', rotation=45)
+	#savefig('images/lab6/feature_selection/' + titl + '_variance_analysis.png', dpi=300, bbox_inches="tight")
+	#clf() # cleanup
 	return lst_variables
 
 def drop_redundant_list(data: pd.DataFrame, vars_2drop) -> pd.DataFrame:
@@ -78,74 +82,72 @@ def drop_redundant_list(data: pd.DataFrame, vars_2drop) -> pd.DataFrame:
 def main():
 	register_matplotlib_converters()
 
+	# ------- VARIANCE ANALYSIS ------- #
 
-	# ------- AIR QUALITY ------- #
-
-	print("------- AIR QUALITY -------")
-
-	data = pd.read_csv('data/air_quality_train_smote.csv', na_values="", parse_dates=True, infer_datetime_format=True)
-	
-	# Variable Correlation Analysis
-	drop, corr_mtx = get_redundant_variables(data.corr(), THRESHOLD)
-	print("------- Redundant Data Variables -------")
-	print(drop.keys())
-	print()
-	get_variable_correlation(corr_mtx, 'air_quality')
-	df = drop_redundant(data, drop)
-	df.to_csv(f'data/air_quality_train_smote_no_correlated_vars.csv', index=False)
-
-	# Variance Analysis with data
+	# AQ
+	# train
+	data = pd.read_csv('data/air_quality_train_smote.csv', na_values='?')
 	numeric = get_variable_types(data)['Numeric']
-	vars_2drop = select_low_variance(data[numeric], 0.1, 'air_quality')
+	vars_2drop = select_low_variance(data[numeric], 1.5, 'air_quality')
+	#print(vars_2drop)
 	data = drop_redundant_list(data, vars_2drop)
 	data.to_csv(f'data/air_quality_train_smote_no_low_variance_vars.csv', index=False)
-	print("------- Data Variables with low variance -------")
-	print(vars_2drop)
-	print()
-
-	# Variance Analysis with df
-	numeric = get_variable_types(df)['Numeric']
-	vars_2drop = select_low_variance(df[numeric], 0.1, 'air_qualitydf')
-	df = drop_redundant_list(df, vars_2drop)
-	df.to_csv(f'data/air_quality_train_smote_no_low_variance_and_no_correlated_vars.csv', index=False)
-	print("------- Df Variables with low variance -------")
-	print(vars_2drop)
-	print()
+	# test
+	data = pd.read_csv('data/air_quality_test.csv', na_values='?')
+	data = drop_redundant_list(data, vars_2drop)
+	data.to_csv(f'data/air_quality_test_no_low_variance_vars.csv', index=False)
 
 
-	# ------- NYC Collisions ------- #
-
-	print("------- NYC COLLISIONS -------")
-
-	data = pd.read_csv('data/NYC_collisions_train_smote.csv', na_values="", parse_dates=True, infer_datetime_format=True)
-
-	# Variable Correlation Analysis
-	drop, corr_mtx = get_redundant_variables(data.corr(), THRESHOLD)
-	print("------- Redundant Data Variables -------")
-	print(drop.keys())
-	print()
-	get_variable_correlation(corr_mtx, 'nyc_collisions')
-	df = drop_redundant(data, drop)
-	df.to_csv(f'data/NYC_collisions_train_smote_no_correlated_vars.csv', index=False)
-
-	# Variance Analysis
+	# NYC
+	# train
+	data = pd.read_csv('data/NYC_collisions_train_smote.csv', na_values='?')
 	numeric = get_variable_types(data)['Numeric']
-	vars_2drop = select_low_variance(data[numeric], 0.1, 'nyc_collisions')
+	vars_2drop = select_low_variance(data[numeric], 0.05, 'nyc_collisions')
+	#print(vars_2drop)
 	data = drop_redundant_list(data, vars_2drop)
 	data.to_csv(f'data/NYC_collisions_train_smote_no_low_variance_vars.csv', index=False)
-	print("------- Data Variables with low variance -------")
-	print(vars_2drop)
-	print()
+	# test
+	data = pd.read_csv('data/NYC_collisions_test.csv', na_values='?')
+	data = drop_redundant_list(data, vars_2drop)
+	data.to_csv(f'data/NYC_collisions_test_no_low_variance_vars.csv', index=False)
 
-	# Variance Analysis with df
-	numeric = get_variable_types(df)['Numeric']
-	vars_2drop = select_low_variance(df[numeric], 0.1, 'nyc_collisionsdf')
-	df = drop_redundant_list(df, vars_2drop)
-	df.to_csv(f'data/NYC_collisions_train_smote_no_low_variance_and_no_correlated_vars.csv', index=False)
-	print("------- Df Variables with low variance -------")
-	print(vars_2drop)
-	print()
+	
+	# ------- CORRELATION ANALYSIS ------- #
 
+	# datasets
+	aq_data_train = pd.read_csv('data/air_quality_train_smote_no_low_variance_vars.csv', na_values='?')
+	aq_data_test = pd.read_csv('data/air_quality_test_no_low_variance_vars.csv', na_values='?')
+
+	nyc_data_train = pd.read_csv('data/NYC_collisions_train_smote_no_low_variance_vars.csv', na_values='?')
+	nyc_data_test = pd.read_csv('data/NYC_collisions_test_no_low_variance_vars.csv', na_values='?')
+
+	for corr_thres in [ .1*x for x in range(0, 10)]:
+
+		# AQ
+		# train
+		drop, corr_mtx = get_redundant_variables(aq_data_train.corr(), corr_thres)
+		get_variable_correlation(corr_mtx, 'air_quality_' + str(corr_thres))
+		df = drop_redundant(aq_data_train, drop)
+		df.to_csv(f'data/air_quality_train_smote_no_low_variance_vars_no_correlated_vars_{corr_thres}.csv', index=False)
+		# test
+		df = drop_redundant(aq_data_test, drop)
+		df.to_csv(f'data/air_quality_test_no_low_variance_vars_no_correlated_vars_{corr_thres}.csv', index=False)
+
+
+		# NYC
+		# train
+		drop, corr_mtx = get_redundant_variables(nyc_data_train.corr(), corr_thres)
+		get_variable_correlation(corr_mtx, 'nyc_collisions_' + str(corr_thres))
+		df = drop_redundant(nyc_data_train, drop)
+		df.to_csv(f'data/NYC_collisions_train_smote_no_low_variance_vars_no_correlated_vars_{corr_thres}.csv', index=False)
+		# test
+		df = drop_redundant(nyc_data_test, drop)
+		df.to_csv(f'data/NYC_collisions_test_no_low_variance_vars_no_correlated_vars_{corr_thres}.csv', index=False)
+
+
+	# ------- CHI-SQUARE ANALYSIS ------- #
+	# TODO
+	
 
 if __name__ == '__main__':
 	main()
